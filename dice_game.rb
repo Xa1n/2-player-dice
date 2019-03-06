a997ddc2eca4a8e3bc2b928d3eb32fad9401e3f3
Xa1n <45832014+Xa1n@users.noreply.github.com>
2019-03-06 18:56:46 +0000

    Add files via upload

---
+++ dice_game.rb
@@ -0,0 +1,26 @@
+player_score = 0
+opponent_score = 0
+
+player_roll = rand(7) + rand(7)
+opponent_roll = rand(7) + rand(7)
+
+puts "You rolled: #{player_roll}"
+puts "Opponent rolled: #{opponent_roll}"
+
+while opponent_score || player_score < 2 do
+
+if player_roll < opponent_roll
+  puts "You lose!"
+  opponent_score = opponent_score + 1
+elsif player_roll == opponent_roll
+  puts "It's a draw!"
+else
+   puts "You win!"
+   player_score = player_score + 1
+end
+
+if opponent_score || player_score == 2
+  break
+end
+
+end
