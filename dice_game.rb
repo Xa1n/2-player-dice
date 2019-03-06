52cc44e8aad2d0b0e818152338f754a44879adb6
Xa1n <45832014+Xa1n@users.noreply.github.com>
2019-03-06 19:09:20 +0000

    Update dice_game.rb
    Added game over message

--- dice_game.rb
+++ dice_game.rb
@@ -7,7 +7,7 @@ opponent_roll = rand(7) + rand(7)
 puts "You rolled: #{player_roll}"
 puts "Opponent rolled: #{opponent_roll}"
 
-while opponent_score || player_score < 2 do
+while opponent_score&&player_score < 2 do
 
 if player_roll < opponent_roll
   puts "You lose!"
@@ -20,6 +20,7 @@ else
 end
 
 if opponent_score || player_score == 2
+  puts "Game over!"
   break
 end
 
