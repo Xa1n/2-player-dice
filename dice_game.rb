03f64e5a19966ebf064500b74d0b78a2b9b30ea0
Xa1n <45832014+Xa1n@users.noreply.github.com>
2019-03-06 19:26:21 +0000

    Update dice_game.rb

--- dice_game.rb
+++ dice_game.rb
@@ -1,22 +1,23 @@
 player_score = 0
 opponent_score = 0
 
-player_roll = rand(7) + rand(7)
-opponent_roll = rand(7) + rand(7)
-
-puts "You rolled: #{player_roll}"
-puts "Opponent rolled: #{opponent_roll}"
 
 while opponent_score&&player_score < 2 do
 
+  player_roll = rand(7) + rand(7)
+  opponent_roll = rand(7) + rand(7)
+
+  puts "You rolled: #{player_roll}"
+  puts "Opponent rolled: #{opponent_roll}"
+
 if player_roll < opponent_roll
   puts "You lose!"
-  opponent_score = opponent_score + 1
+  opponent_score += opponent_score
 elsif player_roll == opponent_roll
   puts "It's a draw!"
 else
    puts "You win!"
-   player_score = player_score + 1
+   player_score += player_score
 end
 
 if opponent_score || player_score == 2
@@ -25,3 +26,6 @@ if opponent_score || player_score == 2
 end
 
 end
+
+p player_score
+p opponent_score
