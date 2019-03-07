6f16d6096811f69932959b4ff28159c29b414064
Xa1n <45832014+Xa1n@users.noreply.github.com>
2019-03-07 16:56:41 +0000

    fixing the loop

--- dice_game.rb
+++ dice_game.rb
@@ -2,7 +2,7 @@ player_score = 0
 opponent_score = 0
 
 
-while opponent_score&&player_score < 2 do
+while true do
 
   player_roll = rand(1..6) + rand(1..6)
   opponent_roll = rand(1..6) + rand(1..6)
@@ -11,17 +11,23 @@ while opponent_score&&player_score < 2 do
   puts "Opponent rolled: #{opponent_roll}"
 
 if player_roll < opponent_roll
-  opponent_score = opponent_score + 1
+  opponent_score += 1
   puts "You lose! Opponent score: #{opponent_score}"
+
 elsif player_roll == opponent_roll
   puts "It's a draw!"
 else
-  player_score = player_score + 1
-   puts "You win! Your score #{player_score}"
+  player_score += 1
+   puts "You win! Your score: #{player_score}"
+
 end
 
-if opponent_score || player_score == 2
+
+if opponent_score == 2 || player_score == 2
   p player_score
   p opponent_score
   break
+
+end
+
 end
